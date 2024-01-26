fd8061f71985c3b277b365bcb325951eb45d892a
Leynardle <romanenko.leonid2007@gmail.com>
2024-01-26 20:41:41 +0200

    fix&add js

--- user.js
+++ user.js
@@ -2,6 +2,8 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
 let profile = document.getElementById('profile');
+let productsGrid = document.getElementById('user-products-grid');
+
 let url = 'https://my-json-server.typicode.com/Leynardle/Fliper';
 
 let userRequest = new XMLHttpRequest();
@@ -18,3 +20,24 @@ userRequest.onload = function () {
     `
 }
 userRequest.send();
+
+let productsRequest = new XMLHttpRequest();
+
+productsRequest.open('GET' `${url}/products?author_id=${id}`);
+productsRequest.responseType = 'json'
+productsRequest.onload = function () {
+    let products = productsRequest.response;
+    productsGrid.innerHTML = null;
+    products.forEach(p => {
+        productsGrid.innerHTML += `
+        <div class="products">
+            <h2 class='products-name'>${p.name}</h2>
+            <img class='products-photo' src='${p.photo_url}' alt='${p.name}'>
+            <p class='products-price'><b>Price:</b>${p.price}$</p>
+            <p class='products-description'><b>Description: </b>${p.description}</p> 
+        </div>
+        `;
+    });
+}
+
+productsRequest.send();
