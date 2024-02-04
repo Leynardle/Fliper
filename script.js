a6cc78028dc5818ca760232ee8d5f1085457a3de
Leynardle <romanenko.leonid2007@gmail.com>
2024-02-04 10:38:43 +0200

    add Leonid Part

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let productsGrid = document.getElementById('products-grid');
 let productsArray = [];
 let xhr = new XMLHttpRequest();
-let url = 'https://my-json-server.typicode.com/Leynardle/Fliper'
+let url = 'https://my-json-server.typicode.com/Leynardle/Fliper';
 
 xhr.open('Get', url + '/products');
 xhr.responseType = 'json'
 xhr.onload = function () {
-    let products = xhr.response;
-    productsGrid.innerHTML = null;
-    products.forEach(p => {
+    productsArray = xhr.response;
+    productsArray.innerHTML = null;
+    productsArray.forEach(p => {
         productsArray.push(p);
         let pElem = document.createElement('div');
         pElem.classList.add('product');
@@ -25,8 +25,35 @@ xhr.onload = function () {
 }
 xhr.send();
 
-function openCart() {
-    cartProd.classList.toggle('hide');
+function addProductToCart(id) {
+    xhr.open('GET', `${url}/products/${id}`);
+    xhr.responseType = 'json'
+    xhr.onload = function () {
+        
     }
+}
 
-let cartProd = document.getElementById('cart-products')
+
+
+let cartProd = document.getElementById('cart-products');
+
+let cart = []
+if (localStorage.getItem('cart')) {
+    cart = json.parse(localStorage.getItem('cart'));
+    drawCartProducts();
+}
+
+
+function addProductToCart(id) {
+    let product = productsArray.find(function (p) {
+        return p.id == id;
+    })
+    cart.push(product);
+    drawCartProducts();
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    document.getElementById('cart-button').classList.add('active');
+    setTimeout(function () {
+        document.getElementById('cart-button').classList.remove('active');
+    }, 500);
+}
