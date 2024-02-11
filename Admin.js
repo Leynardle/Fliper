afb317b2b649bf6a52a9ff2151605bdf63d90ab4
Leynardle <romanenko.leonid2007@gmail.com>
2024-02-11 11:43:39 +0200

    fix

--- Admin.js
+++ Admin.js
@@ -1,38 +1,28 @@
-
-let productForm =
-
-document.getElementById('add_product_form');
+let productForm = document.getElementById('add_product_form');
 
 productForm.addEventListener('submit', function (event) {
-
     event.preventDefault();
-
     let data = JSON.stringify({
-
-    
     "name": event.target ['name'].value,
-
-    "description": event.target ['description'].value, "price": event.target['price'].value,
-
+    "description": event.target['description'].value,
+    "price": event.target['price'].value,
     "photo_url": event.target ['photo_url'].value
 });
 
-
-    let xhr = new XMLHttpRequest (); xhr.withCredentials = false;
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
 
     xhr.addEventListener("readystatechange", function () {
-
-        if (this.readyState === 4) {
-
+    if (this.readyState === 4) {
+            console.log (this.responseText);
         }
-
     });
 
-    console.log (this.responseText);
+    xhr.open("POST", "https://fliper-dc51.restdb.io/rest/product");
 
-    xhr.open("POST", "https://market-6d33.restdb.io/rest/products");
-
-    xhr.setRequestHeader("content-type", "application/json"); xhr.setRequestHeader ("x-apikey", "61b3ca4f72a03f5dae8222ad"); xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("x-apikey", "65c88987c8b82559110da2cb");
+    xhr.setRequestHeader("cache-control", "no-cache");
 
     xhr.send (data);
 
