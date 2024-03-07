1e570bc63374c693129eb6086968a3517cd34692
Leynardle <romanenko.leonid2007@gmail.com>
2024-03-07 18:42:57 +0200

    fix

--- admin.js
+++ admin.js
@@ -12,11 +12,15 @@ productForm.addEventListener('submit', function (event) {
     let xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
 
-    xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-            console.log (this.responseText);
+    xhr.onload = function () {
+        if (xhr.status === 201) {
+            event.target.reset();
+            alert('Product added successfully');
         }
-    });
+        else{
+            alert('Server error. Try again later')
+        }
+    };
 
     xhr.open("POST", "https://fliper-dc51.restdb.io/rest/product");
 
@@ -30,12 +34,14 @@ productForm.addEventListener('submit', function (event) {
 let orders = document.getElementById("admin_page_orders")
 
 let xhr = new XMLHttpRequest();
-xhr.open("GET", "https://fliper-dc51.restdb.io/rest/product"); xhr.responseType = 'json'
+xhr.open("GET", "https://fliper-dc51.restdb.io/rest/product");
+xhr.responseType = 'json'
 xhr.setRequestHeader("content-type", "application/json"); 
 xhr.setRequestHeader("x-apikey", "65c88987c8b82559110da2cb"); 
 xhr.setRequestHeader("cache-control", "no-cache");
-xhr.onload = function() {
 
+
+xhr.onload = function() {
     xhr.response.forEach(function(order){
         let orderElement = document.createElement('div');
         orderElement.classList.add('product');
@@ -43,28 +49,29 @@ xhr.onload = function() {
         if (order.status == 'Completed') {
             statusColor = 'yellow';
     }
-orderElement.innerHTML +=`<h2>Order ${order._id}</h2>
-<p><b>Status:</b> <span style="color:${statusColor}">${order.status}</span></p> 
-<p><b>Customer name: </b> ${order.name}</p>
-<p><b>Address:</b> ${order.address}</p>
-<p><b>Phone:</b> ${order.phone}</p>
-<p><b>Post Office Number:</b> ${order.post_number}</p>`;
-
-let sum = 0;
-
-
-order.products.forEach(function(p){
-
-});
-
-orderElement.innerHTML +=`<p><img height="50" src="${p.photo_url}"> ${p.name} |${p.price}$</p>`;
-sum += +p.price
-orderElement.innerHTML +=`
-<p>Total Price: ${sum}</p>
-<button onclick="complete('${order._id}')">Mark as Completed</button>`;
-orders.append(orderElement);
-   });
+    orderElement.innerHTML += `
+        <h2>Order ${order._id}</h2>
+        <p><b>Status:</b> <span style="color:${statusColor}">${order.status}</span></p> 
+        <p><b>Customer name: </b> ${order.name}</p>
+        <p><b>Address:</b> ${order.address}</p>
+        <p><b>Phone:</b> ${order.phone}</p>
+        <p><b>Post Office Number:</b> ${order.post_number}</p>
+    `;
+    let sum = 0;
+    order.products.forEach(function (p) {
+        orderElement.innerHTML += `
+            <p><img height="50" src="${p.photo_url}"> ${p.name} |${p.price}$</p>
+        `;
+        sum += +p.price
+    });
+    orderElement.innerHTML +=`
+    <p>Total Price: ${sum}</p>
+    <button onclick="complete('${order._id}')">Mark as Completed</button>
+    `;
+    orders.append(orderElement);
+   })
 }
+
 xhr.send();
 
 function complete(id) {
@@ -78,18 +85,15 @@ function complete(id) {
         if (xhr.status == 200) {
             location.reload();
         }
-            else {
-alert('Server error. Try again later');
-            }
+        else {
+            alert('Server error. Try again later');
         }
-
-xhr.open("PUT", "https://fliper-dc51.restdb.io/rest/product"+id);
-
-xhr.setRequestHeader("content-type", "application/json"); 
-
-xhr.setRequestHeader("x-apikey", "65c88987c8b82559110da2cb"); 
-
-xhr.setRequestHeader("cache-control", "no-cache");
-
-xhr.send(data);
     }
+
+    xhr.open("PUT", "https://fliper-dc51.restdb.io/rest/product"+id);
+    xhr.setRequestHeader("content-type", "application/json"); 
+    xhr.setRequestHeader("x-apikey", "65c88987c8b82559110da2cb"); 
+    xhr.setRequestHeader("cache-control", "no-cache");
+
+    xhr.send(data);
+}
