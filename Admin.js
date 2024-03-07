d46b979a74d71de114e3afa00a74e92a9eb93d32
Nikita <nikitisik2019@gmail.com>
2024-03-07 17:09:35 +0100

    js admin

--- Admin.js
+++ Admin.js
@@ -27,3 +27,78 @@ productForm.addEventListener('submit', function (event) {
     xhr.send (data);
 
 })
+
+let xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://market-6d33.restdb.io/rest/orders"); xhr.responseType = 'json'
+
+xhr.setRequestHeader("content-type", "application/json"); xhr.setRequestHeader("x-apikey", "61b3ca4f72a03f5dae8222ad"); xhr.setRequestHeader("cache-control", "no-cache");
+
+xhr.onload = function() {
+
+    xhr.response.forEach(function(order){
+
+        let orderElement = document.createElement('div');
+
+        orderElement.classList.add('product');
+
+        let statusColor = 'green';
+
+        if (order.status == 'Completed') {
+
+            statusColor = 'yellow';
+    }
+orderElement.innerHTML +=`<h2>Order ${order._id}</h2>
+
+<p><b>Status:</b> <span style="color:${statusColor}">${order.status}</span></p> <p><b>Customer name: </b> ${order.name}</p>
+
+let orders document.getElementById('admin_page_orders');
+xhr.send();
+<p><b>Address:</b> ${order.address}</p>
+<p><b>Phone:</b> ${order.phone}</p>
+<p><b>Post Office Number:</b> ${order.post_number}</p>`;
+
+let sum = 0;
+
+order.products.forEach(function(p){
+
+});
+
+orderElement.innerHTML +=`<p><img height="50" src="${p.photo_url}"> ${p.name} |${p.price}$</p>`;
+
+sum +=+p.price
+
+orderElement.innerHTML +=`<p>Total Price: ${sum}</p>
+
+<button onclick="complete('${order._id}')">Mark as Completed</button>`;
+
+orders.append(orderElement);
+
+   });
+}
+xhr.send();
+
+
+function complete(id) {
+    var data = JSON.stringify({
+        "status": "Completed"
+    });
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+
+    xhr.onload = function() {
+        if (xhr.status == 200) {
+            location.reload();
+        }
+            else {
+alert('Server error. Try again later');
+            }
+        }
+
+xhr.open("PUT", "https://market-6d33.restdb.io/rest/orders/"+id);
+xhr.setRequestHeader("content-type", "application/json"); 
+xhr.setRequestHeader("x-apikey", "61b3ca4f72a03f5dae8222ad"); 
+xhr.setRequestHeader("cache-control", "no-cache");
+
+xhr.send(data);
+    }
